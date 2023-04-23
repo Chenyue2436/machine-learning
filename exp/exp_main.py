390be26270965ac3a5922898579b13e76b09c09e
Chenyue2436 <619792234@qq.com>
2023-04-23 14:43:38 +0800

    t

---
+++ exp/exp_main.py
@@ -0,0 +1,165 @@
+from data_provider.data_factory import data_provider
+from exp.exp_basic import Exp_Basic
+from models import Test, DLinear, SCINet, DLSNet, MLP, DMLP, MLPN
+from utils.tools import EarlyStopping, adjust_learning_rate, visual
+from utils.metrics import metric
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+import os
+import time
+import warnings
+import matplotlib.pyplot as plt
+
+warnings.filterwarnings('ignore')
+
+class Exp_Main(Exp_Basic):
+    def __init__(self, args):
+        super(Exp_Main, self).__init__(args)
+
+    def _build_model(self):
+        model_dict = {'Test': Test, 'DLinear': DLinear, 'SCINet': SCINet, 'DLSNet': DLSNet, 'MLP': MLP, 'DMLP': DMLP, 'MLPN': MLPN}
+        model = model_dict[self.args.model].Model(self.args).float()
+        if self.args.use_multi_gpu and self.args.use_gpu:
+            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        return model
+
+    def _get_data(self, flag):
+        data_set, data_loader = data_provider(self.args, flag)
+        return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        return model_optim
+
+    def _select_criterion(self):
+        if self.args.loss == 'mse':
+            criterion = nn.MSELoss()
+        else:
+            criterion = nn.MAELoss()
+        return criterion
+
+    def vali(self, vali_data, vali_loader, criterion):
+        total_loss = []
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, _, _) in enumerate(vali_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+                
+                outputs = self.model(batch_x)                                                  
+                
+                if self.args.inverse:
+                    outputs = vali_data.inverse_transform(outputs)
+                    batch_y = vali_data.inverse_transform(batch_y)  
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                pred = outputs.detach().cpu()
+                true = batch_y.detach().cpu()
+                loss = criterion(pred, true)
+                total_loss.append(loss)
+        total_loss = np.average(total_loss)
+        self.model.train()
+        return total_loss
+
+    def train(self, setting):
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
+        path = os.path.join(self.args.checkpoints, setting)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+        for epoch in range(self.args.train_epochs):
+            iter_count = 0
+            train_loss = []
+            self.model.train()
+            epoch_time = time.time()
+            for i, (batch_x, batch_y, _, _) in enumerate(train_loader):          
+                iter_count += 1
+                model_optim.zero_grad()
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                outputs = self.model(batch_x)
+
+                if self.args.inverse:
+                    outputs = train_data.inverse_transform(outputs)
+                    batch_x = train_data.inverse_transform(batch_x)
+                    batch_y = train_data.inverse_transform(batch_y)
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_x = batch_x[:, -self.args.seq_len:, f_dim:].to(self.device)
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                loss = criterion(outputs, batch_y)
+                train_loss.append(loss.item())
+                loss.backward()
+                model_optim.step()
+            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+            train_loss = np.average(train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion)
+            test_loss = self.vali(test_data, test_loader, criterion)
+            print("Epoch: {0} | Train Loss: {1:.7f} Vali Loss: {2:.7f} Test Loss: {3:.7f}".format(epoch + 1, train_loss, vali_loss, test_loss))
+            early_stopping(vali_loss, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+            adjust_learning_rate(model_optim, epoch + 1, self.args)
+        best_model_path = path + '/' + 'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+        return self.model
+
+    def test(self, setting):
+        test_data, test_loader = self._get_data(flag='test')
+        inputs = []
+        preds = []
+        trues = []
+        folder_path = './test_results/' + setting + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, _, _) in enumerate(test_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+                
+                outputs = self.model(batch_x)
+                    
+                if self.args.inverse:
+                    outputs = test_data.inverse_transform(outputs)
+                    batch_x = test_data.inverse_transform(batch_x)
+                    batch_y = test_data.inverse_transform(batch_y)
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_x = batch_x[:, -self.args.seq_len:, f_dim:].to(self.device)
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)                
+                outputs = outputs.detach().cpu().numpy()
+                batch_x = batch_x.detach().cpu().numpy()
+                batch_y = batch_y.detach().cpu().numpy()
+                pred = outputs
+                input = batch_x
+                true = batch_y  
+                inputs.append(input)
+                preds.append(pred)
+                trues.append(true)
+                if i % 20 == 0:
+                    gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
+                    pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
+                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
+        inputs = np.array(inputs)
+        preds = np.array(preds)
+        trues = np.array(trues)
+        inputs = inputs.reshape(-1, inputs.shape[-2], inputs.shape[-1])
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        mae, mse, rmse, mape, mspe, rse, corr, mae_last, mse_last = metric(preds, trues, inputs)
+        print('mse:{}, mae:{}'.format(mse, mae))
+        np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
+        np.save(folder_path + 'pred.npy', preds)
+        np.save(folder_path + 'true.npy', trues)
+        np.save(folder_path + 'input.npy', inputs)
+        return mse, mae, mse_last, mae_last
